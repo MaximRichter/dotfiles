2f94a7cab81d1bf46662475dd99a721c435f2f44
MaximRichter <andemirkan07@gmail.com>
2025-05-28 18:48:03 +0300

    changed todo-comments options

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -20,7 +20,7 @@ return {
 		"folke/todo-comments.nvim",
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
+		opts = {},
 	},
 	{
 		-- high-performance color highlighter
